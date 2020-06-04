a700e1d8b00058b51d823c86a373836605823793
Jose Franco <jose.franco@semperti.com>
2020-06-03 23:23:23 -0300

    changes

--- MyAppWeb/Program.cs
+++ MyAppWeb/Program.cs
@@ -12,7 +12,7 @@ namespace MyAppWeb
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             CreateHostBuilder(args).Build().Run();
         }
 
